c317e3c790673669e2fd61d2e6365f96ac7b873b
yangguanghui <yangguanghui@beisen.com>
2019-12-10 11:56:52 +0800

    简单修改

--- SrtTranslator/MainForm.cs
+++ SrtTranslator/MainForm.cs
@@ -17,37 +17,48 @@ namespace SrtTranslator
         private void btnOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            if(ofd.ShowDialog()==DialogResult.OK)
+            ofd.Filter = "字幕文件（*.srt）|*.srt|全部文件（*.*）|*.*";
+            if (ofd.ShowDialog()==DialogResult.OK)
             {
                 string filePath = ofd.FileName;
-                Srt srt = new Srt(filePath);
-                string subtitleAll = srt.GetAllSubtitle();
-                string translateText = YouDaoTranslator.Translate(subtitleAll); //Translater.Translate(subtitleAll);
-                textBoxResult.Text = translateText.Replace("。", Environment.NewLine);
-                string[] translateTexts = translateText.Split('。');
-                int insertId = 0;
-                int tId = 0;
-                for (int i=0; i<srt.Count();i++)
+                TranslateSrt(filePath);
+            }
+        }
+
+        /// <summary>
+        /// 翻译字幕文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        private void TranslateSrt(string filePath)
+        {
+            Srt srt = new Srt(filePath);
+            string subtitleAll = srt.GetAllSubtitle();
+            string[] rawTexts = subtitleAll.Split('.');
+            string translateText = YouDaoTranslator.Translate(subtitleAll); //Translater.Translate(subtitleAll);
+            //textBoxResult.Text = translateText.Replace("。", Environment.NewLine);
+            string[] translateTexts = translateText.Split('。');
+            int insertId = 0;
+            int tId = 0;
+            for (int i = 0; i < srt.Count(); i++)
+            {
+                if (srt[i].Subtitles.Any(x => x.Contains(".")) && tId < translateTexts.Length)
                 {
-                    if(srt[i].Subtitles.Any(x=>x.Contains("."))&& tId < translateTexts.Length)
-                    {
-                        srt[insertId].TranslateText = new List<string>() { translateTexts[tId] };
-                        if (srt[i].Subtitles.Any(x => x.EndsWith(".")))
-                            insertId = i + 1;
-                        else
-                            insertId = i;
-                        tId++;
-                    }
+                    srt[insertId].TranslateText = new List<string>() { translateTexts[tId] };
+                    if (srt[i].Subtitles.Any(x => x.EndsWith(".")))
+                        insertId = i + 1;
+                    else
+                        insertId = i;
+                    tId++;
                 }
-                textBox1.Clear();
-                for (int i = 0; i < srt.Count(); i++)
-                {
-                    textBox1.AppendText(srt[i].Index + Environment.NewLine);
-                    textBox1.AppendText(srt[i].Time + Environment.NewLine);
-                    textBox1.AppendText(String.Join(Environment.NewLine, srt[i].Subtitles.ToArray()) + Environment.NewLine);
-                    if (srt[i].TranslateText?.Count > 0) textBox1.AppendText(String.Join(Environment.NewLine, srt[i].TranslateText.ToArray()) + Environment.NewLine);
-                    textBox1.AppendText(Environment.NewLine);
-                }
+            }
+            textBox1.Clear();
+            for (int i = 0; i < srt.Count(); i++)
+            {
+                textBox1.AppendText(srt[i].Index + Environment.NewLine);
+                textBox1.AppendText(srt[i].Time + Environment.NewLine);
+                textBox1.AppendText(String.Join(Environment.NewLine, srt[i].Subtitles.ToArray()) + Environment.NewLine);
+                if (srt[i].TranslateText?.Count > 0) textBox1.AppendText(String.Join(Environment.NewLine, srt[i].TranslateText.ToArray()) + Environment.NewLine);
+                textBox1.AppendText(Environment.NewLine);
             }
         }
 
